6263d54758157c39ab17174a47df95569049ecb6
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-08-22 11:18:51 +0500

    A function created for looping xoxo for 9 times

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let gameBoard = ["","","","","","","","",""]
+const gameBoard = ["","","","","","","","",""]
 
+function ticTacToe(){
+    function choosingOption(){
+        const keys = ['x','o'];
+        let result = ''
+
+        for (let i = 0; i < 9; i++){
+            result += keys[i % keys.length]
+        }
+
+        console.log(result);
+        
+    }
+    choosingOption();
+}
+
+ticTacToe();
