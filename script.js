1882d4327c58317feec41111ce14506b7d896628
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-09-04 11:48:40 +0500

    Created conditions for checking winner!!

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-const gameBoard = ["","","","","","","","",""]
+   const gameBoard = Array(9).fill(null);   
 
-function ticTacToe(){
-    function choosingOption(){
-        const keys = ['x','o'];
-        let result = ''
+    function ticTacToe(){
+        const winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [6,4,2], [0,4,8]];
 
-        for (let i = 0; i < 9; i++){
-            result += keys[i % keys.length]
+        function printBoard(){
+
+            console.log(`
+            ${gameBoard[0] || '-'} | ${gameBoard[1] || '-'} | ${gameBoard[2] || '-'}
+            ---------
+            ${gameBoard[3] || '-'} | ${gameBoard[4] || '-'} | ${gameBoard[5] || '-'}
+            ---------
+            ${gameBoard[6] || '-'} | ${gameBoard[7] || '-'} | ${gameBoard[8] || '-'}`)
         }
+        printBoard();
 
-        console.log(result);
-        
+        function checkWinner(winningPatterns){
+            for(const pattern of winningPatterns){
+                const a = pattern[0];
+                const b = pattern[1];
+                const c = pattern[2];
+
+                if(gameBoard[a] === null){
+                    continue;
+                }
+
+                if(gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
+                    return gameBoard[a];
+                }
+            }
+        }
     }
-    choosingOption();
-}
 
-ticTacToe();
+    ticTacToe();
